75c166177ced84e046d9ff99c9d20be9cc3849b3
Octavio Pareja <octaviopareja05@gmail.com>
2021-05-13 16:23:47 -0300

    avances
    .

--- src/Pages/Landing/Step4.js
+++ src/Pages/Landing/Step4.js
@@ -1,5 +1,7 @@
 import { Col, Row, Container } from "react-bootstrap";
 
+import spotify from "../../assets/img/spotify.jpg";
+
 const Step4 = () => {
   return (
     <Container className="mx-auto workoutPlaylist">
@@ -11,7 +13,9 @@ const Step4 = () => {
       </Row>
       <Row>
         <Col md={12}>
-          <div></div>
+          <div className="pt-5 pb-5">
+            <img src={spotify} />
+          </div>
         </Col>
       </Row>
     </Container>
--- src/Pages/Landing/Step5.js
+++ src/Pages/Landing/Step5.js
@@ -1,4 +1,4 @@
-import { Col, Row, Container } from "react-bootstrap";
+import { Col, Row, Container, Tab, Tabs } from "react-bootstrap";
 
 const Step5 = () => {
   return (
@@ -11,7 +11,152 @@ const Step5 = () => {
       </Row>
       <Row>
         <Col md={12}>
-          <div className="boxHistory">asd</div>
+          <div className="boxHistory">
+            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+              <Tab eventKey="home" title="Work Experience">
+                <Row>
+                  <Col md={12}>
+                    <ul class="timeline">
+                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
+                        <div class="timeline-arrow"></div>
+                        <h2 class="h5 mb-0">Title of section 1</h2>
+                        <span class="small text-gray">
+                          <i class="fa fa-clock-o mr-1"></i>21 March, 2019
+                        </span>
+                        <p class="text-small mt-2 font-weight-light">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Quisque scelerisque diam non nisi semper, et
+                          elementum lorem ornare. Maecenas placerat facilisis
+                          mollis. Duis sagittis ligula in sodales vehicula....
+                        </p>
+                      </li>
+                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
+                        <div class="timeline-arrow"></div>
+                        <h2 class="h5 mb-0">Title of section 2</h2>
+                        <span class="small text-gray">
+                          <i class="fa fa-clock-o mr-1"></i>5 April, 2019
+                        </span>
+                        <p class="text-small mt-2 font-weight-light">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Quisque scelerisque diam non nisi semper.
+                        </p>
+                        <p class="text-small mt-2 font-weight-light">
+                          Libero expedita explicabo eius fugiat quia aspernatur
+                          autem laudantium error architecto recusandae natus
+                          sapiente sit nam eaque, consectetur porro molestiae
+                          ipsam! Deleniti.
+                        </p>
+                      </li>
+                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
+                        <div class="timeline-arrow"></div>
+                        <h2 class="h5 mb-0">Title of section 3</h2>
+                        <span class="small text-gray">
+                          <i class="fa fa-clock-o mr-1"></i>18 August, 2019
+                        </span>
+                        <p class="text-small mt-2 font-weight-light">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Quisque scelerisque diam non nisi semper, et
+                          elementum lorem ornare. Maecenas placerat facilisis
+                          mollis. Duis sagittis ligula in sodales vehicula....
+                        </p>
+                      </li>
+                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
+                        <div class="timeline-arrow"></div>
+                        <h2 class="h5 mb-0">Title of section 4</h2>
+                        <span class="small text-gray">
+                          <i class="fa fa-clock-o mr-1"></i>10 October, 2019
+                        </span>
+                        <p class="text-small mt-2 font-weight-light">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Quisque scelerisque diam non nisi semper, et
+                          elementum lorem ornare. Maecenas placerat facilisis
+                          mollis. Duis sagittis ligula in sodales vehicula....
+                        </p>
+                        <p class="text-small mt-2 font-weight-light">
+                          Voluptatibus temporibus esse illum eum aspernatur,
+                          fugiat suscipit natus! Eum corporis illum nihil
+                          officiis tempore. Excepturi illo natus libero sit
+                          doloremque, laborum molestias rerum pariatur quam
+                          ipsam necessitatibus incidunt, explicabo.
+                        </p>
+                      </li>
+                    </ul>
+                  </Col>
+                </Row>
+              </Tab>
+              <Tab eventKey="profile" title="Education & Certification">
+                <Row>
+                  <Col md={12}>
+                    <ul class="timeline">
+                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
+                        <div class="timeline-arrow"></div>
+                        <h2 class="h5 mb-0">Title of section 1</h2>
+                        <span class="small text-gray">
+                          <i class="fa fa-clock-o mr-1"></i>21 March, 2019
+                        </span>
+                        <p class="text-small mt-2 font-weight-light">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Quisque scelerisque diam non nisi semper, et
+                          elementum lorem ornare. Maecenas placerat facilisis
+                          mollis. Duis sagittis ligula in sodales vehicula....
+                        </p>
+                      </li>
+                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
+                        <div class="timeline-arrow"></div>
+                        <h2 class="h5 mb-0">Title of section 2</h2>
+                        <span class="small text-gray">
+                          <i class="fa fa-clock-o mr-1"></i>5 April, 2019
+                        </span>
+                        <p class="text-small mt-2 font-weight-light">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Quisque scelerisque diam non nisi semper.
+                        </p>
+                        <p class="text-small mt-2 font-weight-light">
+                          Libero expedita explicabo eius fugiat quia aspernatur
+                          autem laudantium error architecto recusandae natus
+                          sapiente sit nam eaque, consectetur porro molestiae
+                          ipsam! Deleniti.
+                        </p>
+                      </li>
+                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
+                        <div class="timeline-arrow"></div>
+                        <h2 class="h5 mb-0">Title of section 3</h2>
+                        <span class="small text-gray">
+                          <i class="fa fa-clock-o mr-1"></i>18 August, 2019
+                        </span>
+                        <p class="text-small mt-2 font-weight-light">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Quisque scelerisque diam non nisi semper, et
+                          elementum lorem ornare. Maecenas placerat facilisis
+                          mollis. Duis sagittis ligula in sodales vehicula....
+                        </p>
+                      </li>
+                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
+                        <div class="timeline-arrow"></div>
+                        <h2 class="h5 mb-0">Title of section 4</h2>
+                        <span class="small text-gray">
+                          <i class="fa fa-clock-o mr-1"></i>10 October, 2019
+                        </span>
+                        <p class="text-small mt-2 font-weight-light">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Quisque scelerisque diam non nisi semper, et
+                          elementum lorem ornare. Maecenas placerat facilisis
+                          mollis. Duis sagittis ligula in sodales vehicula....
+                        </p>
+                        <p class="text-small mt-2 font-weight-light">
+                          Voluptatibus temporibus esse illum eum aspernatur,
+                          fugiat suscipit natus! Eum corporis illum nihil
+                          officiis tempore. Excepturi illo natus libero sit
+                          doloremque, laborum molestias rerum pariatur quam
+                          ipsam necessitatibus incidunt, explicabo.
+                        </p>
+                      </li>
+                    </ul>
+                  </Col>
+                </Row>
+              </Tab>
+            </Tabs>
+          </div>
         </Col>
       </Row>
     </Container>
