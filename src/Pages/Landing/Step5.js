a40e2b52481cd852c3867fbd3b7e129373d1f885
Octavio Pareja <octaviopareja05@gmail.com>
2021-05-18 22:31:18 -0300

    Fixes
    .

--- src/Pages/Landing/Step5.js
+++ src/Pages/Landing/Step5.js
@@ -1,6 +1,6 @@
 import { Col, Row, Container, Tab, Tabs } from "react-bootstrap";
 
-const Step5 = () => {
+const Step5 = ({ trainerData }) => {
   return (
     <Container className="mx-auto myExperience">
       <Row>
@@ -13,148 +13,46 @@ const Step5 = () => {
         <Col md={12}>
           <div className="boxHistory">
             <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-              <Tab eventKey="home" title="Work Experience">
-                <Row>
-                  <Col md={12}>
-                    <ul class="timeline">
-                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
-                        <div class="timeline-arrow"></div>
-                        <h2 class="h5 mb-0">Title of section 1</h2>
-                        <span class="small text-gray">
-                          <i class="fa fa-clock-o mr-1"></i>21 March, 2019
-                        </span>
-                        <p class="text-small mt-2 font-weight-light">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit. Quisque scelerisque diam non nisi semper, et
-                          elementum lorem ornare. Maecenas placerat facilisis
-                          mollis. Duis sagittis ligula in sodales vehicula....
-                        </p>
-                      </li>
-                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
-                        <div class="timeline-arrow"></div>
-                        <h2 class="h5 mb-0">Title of section 2</h2>
-                        <span class="small text-gray">
-                          <i class="fa fa-clock-o mr-1"></i>5 April, 2019
-                        </span>
-                        <p class="text-small mt-2 font-weight-light">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit. Quisque scelerisque diam non nisi semper.
-                        </p>
-                        <p class="text-small mt-2 font-weight-light">
-                          Libero expedita explicabo eius fugiat quia aspernatur
-                          autem laudantium error architecto recusandae natus
-                          sapiente sit nam eaque, consectetur porro molestiae
-                          ipsam! Deleniti.
-                        </p>
-                      </li>
-                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
-                        <div class="timeline-arrow"></div>
-                        <h2 class="h5 mb-0">Title of section 3</h2>
-                        <span class="small text-gray">
-                          <i class="fa fa-clock-o mr-1"></i>18 August, 2019
-                        </span>
-                        <p class="text-small mt-2 font-weight-light">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit. Quisque scelerisque diam non nisi semper, et
-                          elementum lorem ornare. Maecenas placerat facilisis
-                          mollis. Duis sagittis ligula in sodales vehicula....
-                        </p>
-                      </li>
-                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
-                        <div class="timeline-arrow"></div>
-                        <h2 class="h5 mb-0">Title of section 4</h2>
-                        <span class="small text-gray">
-                          <i class="fa fa-clock-o mr-1"></i>10 October, 2019
-                        </span>
-                        <p class="text-small mt-2 font-weight-light">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit. Quisque scelerisque diam non nisi semper, et
-                          elementum lorem ornare. Maecenas placerat facilisis
-                          mollis. Duis sagittis ligula in sodales vehicula....
-                        </p>
-                        <p class="text-small mt-2 font-weight-light">
-                          Voluptatibus temporibus esse illum eum aspernatur,
-                          fugiat suscipit natus! Eum corporis illum nihil
-                          officiis tempore. Excepturi illo natus libero sit
-                          doloremque, laborum molestias rerum pariatur quam
-                          ipsam necessitatibus incidunt, explicabo.
-                        </p>
-                      </li>
-                    </ul>
-                  </Col>
-                </Row>
-              </Tab>
-              <Tab eventKey="profile" title="Education & Certification">
-                <Row>
-                  <Col md={12}>
-                    <ul class="timeline">
-                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
-                        <div class="timeline-arrow"></div>
-                        <h2 class="h5 mb-0">Title of section 1</h2>
-                        <span class="small text-gray">
-                          <i class="fa fa-clock-o mr-1"></i>21 March, 2019
-                        </span>
-                        <p class="text-small mt-2 font-weight-light">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit. Quisque scelerisque diam non nisi semper, et
-                          elementum lorem ornare. Maecenas placerat facilisis
-                          mollis. Duis sagittis ligula in sodales vehicula....
-                        </p>
-                      </li>
-                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
-                        <div class="timeline-arrow"></div>
-                        <h2 class="h5 mb-0">Title of section 2</h2>
-                        <span class="small text-gray">
-                          <i class="fa fa-clock-o mr-1"></i>5 April, 2019
-                        </span>
-                        <p class="text-small mt-2 font-weight-light">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit. Quisque scelerisque diam non nisi semper.
-                        </p>
-                        <p class="text-small mt-2 font-weight-light">
-                          Libero expedita explicabo eius fugiat quia aspernatur
-                          autem laudantium error architecto recusandae natus
-                          sapiente sit nam eaque, consectetur porro molestiae
-                          ipsam! Deleniti.
-                        </p>
-                      </li>
-                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
-                        <div class="timeline-arrow"></div>
-                        <h2 class="h5 mb-0">Title of section 3</h2>
-                        <span class="small text-gray">
-                          <i class="fa fa-clock-o mr-1"></i>18 August, 2019
-                        </span>
-                        <p class="text-small mt-2 font-weight-light">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit. Quisque scelerisque diam non nisi semper, et
-                          elementum lorem ornare. Maecenas placerat facilisis
-                          mollis. Duis sagittis ligula in sodales vehicula....
-                        </p>
-                      </li>
-                      <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
-                        <div class="timeline-arrow"></div>
-                        <h2 class="h5 mb-0">Title of section 4</h2>
-                        <span class="small text-gray">
-                          <i class="fa fa-clock-o mr-1"></i>10 October, 2019
-                        </span>
-                        <p class="text-small mt-2 font-weight-light">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit. Quisque scelerisque diam non nisi semper, et
-                          elementum lorem ornare. Maecenas placerat facilisis
-                          mollis. Duis sagittis ligula in sodales vehicula....
-                        </p>
-                        <p class="text-small mt-2 font-weight-light">
-                          Voluptatibus temporibus esse illum eum aspernatur,
-                          fugiat suscipit natus! Eum corporis illum nihil
-                          officiis tempore. Excepturi illo natus libero sit
-                          doloremque, laborum molestias rerum pariatur quam
-                          ipsam necessitatibus incidunt, explicabo.
-                        </p>
-                      </li>
-                    </ul>
-                  </Col>
-                </Row>
-              </Tab>
+              {trainerData.experiences && (
+                <Tab eventKey="home" title="Education & Certification">
+                  <Row>
+                    <Col md={12}>
+                      <ul class="timeline">
+                        {trainerData.experiences.map((experience) => (
+                          <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
+                            <div class="timeline-arrow"></div>
+                            <h2 class="h5 mb-0">{experience.course_name}</h2>
+                            <span class="small text-gray">
+                              <i class="fa fa-clock-o mr-1"></i>
+                              {experience.institution}
+                            </span>
+                          </li>
+                        ))}
+                      </ul>
+                    </Col>
+                  </Row>
+                </Tab>
+              )}
+              {trainerData.experiences && (
+                <Tab eventKey="profile" title="Work Experience">
+                  <Row>
+                    <Col md={12}>
+                      <ul class="timeline">
+                        {trainerData.experiences.map((experience) => (
+                          <li class="timeline-item bg-white rounded ml-3 p-4 shadow">
+                            <div class="timeline-arrow"></div>
+                            <h2 class="h5 mb-0">{experience.course_name}</h2>
+                            <span class="small text-gray">
+                              <i class="fa fa-clock-o mr-1"></i>
+                              {experience.institution}
+                            </span>
+                          </li>
+                        ))}
+                      </ul>
+                    </Col>
+                  </Row>
+                </Tab>
+              )}
             </Tabs>
           </div>
         </Col>
